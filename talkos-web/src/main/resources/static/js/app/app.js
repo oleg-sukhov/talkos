297ca363b61974e12e3a66d4817906b82450a68a
oleg_sukhov <oleg.sukhov@email.ua>
2015-01-15 02:08:50 +0200

    Design login page

--- talkos-web/src/main/resources/static/js/app/app.js
+++ talkos-web/src/main/resources/static/js/app/app.js
@@ -1,1 +1,15 @@
-var talkos = angular.module('talkos', ['ngRoute']);
+var talkos = angular.module('talkos', [
+    'ngRoute'
+]);
+
+talkos.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/login', {
+                templateUrl: 'js/app/login/login.html',
+                controller: 'LoginController'
+            }).
+            otherwise({
+                redirectTo: '/login'
+            });
+    }]);
