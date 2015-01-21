5c57ffda0c25e4fbf775b6cc6813b57843ca64f6
oleg_sukhov <oleg.sukhov@email.ua>
2015-01-21 23:23:59 +0200

    UserRepositoryTest

--- talkos-repository/src/test/java/ua/vn/talkos/repository/UserRepositoryTest.java
+++ talkos-repository/src/test/java/ua/vn/talkos/repository/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package ua.vn.talkos.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ua.vn.talkos.config.RepositoryTestConfig;
 import ua.vn.talkos.persistence.UserRepository;
@@ -15,4 +16,15 @@ public class UserRepositoryTest extends AbstractTestNGSpringContextTests {
 
     @Autowired
     private UserRepository userRepository;
+
+    @BeforeClass
+    public void setup() {
+        System.out.println(userRepository);
+
+    }
+
+    @Test
+    public void test() {
+
+    }
 }
