1ae3199bc0350f392bd516f2cb3d42bed08e356d
oleg.sukhov <oleg.sukhov@email.ua>
2015-02-11 19:04:50 +0200

    Change User bean

--- talkos-repository/src/main/java/ua/vn/talkos/entity/User.java
+++ talkos-repository/src/main/java/ua/vn/talkos/entity/User.java
@@ -1,15 +1,12 @@
 package ua.vn.talkos.entity;
 
-import lombok.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.data.jpa.domain.AbstractPersistable;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Collection;
 
 /**
  * @author oleg.sukhov
@@ -18,7 +15,7 @@ import java.util.Collection;
 @Table(name = "users")
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class User extends AbstractPersistable<Long> implements UserDetails {
+public class User extends AbstractPersistable<Long> {
 
     @Column
     private String username;
@@ -28,24 +25,4 @@ public class User extends AbstractPersistable<Long> implements UserDetails {
 
     @Column
     private boolean enabled;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
 }
