1276b48ce5b3a27811aee64924bd50132e0d63ec
oleg_sukhov <oleg.sukhov@email.ua>
2015-03-05 00:20:39 +0200

    Login page validation

--- talkos-web/src/main/webapp/resources/js/app/login/login.controller.js
+++ talkos-web/src/main/webapp/resources/js/app/login/login.controller.js
@@ -3,6 +3,18 @@ angular.module('talkos')
         function ($scope, $location, AuthenticationService) {
 
             $scope.credentials = {};
+            $scope.activeErrors = [];
+            var validationMessages = {
+                'usernameValidation': {
+                    message: "Username can't be empty",
+                    placement: 'right'
+                },
+                'passwordValidation': {
+                    message: "Password can't be empty",
+                    placement: 'right'
+                }
+            };
+
             $scope.login = function () {
                 AuthenticationService.login($scope, '/home');
             };
@@ -13,11 +25,27 @@ angular.module('talkos')
                 $location.path("/registration");
             };
 
-            $scope.$watch('credentials.username', function (newVal, oldVal) {
-                $scope.signInState = !!newVal;
-                $scope.tooltip = {
-                    title: "Username can't be empty",
-                    placement: 'right'
-                };
+            $scope.$watch('credentials.username', function (value) {
+                $scope.validate(value, 'usernameValidation');
             });
+
+            $scope.$watch('credentials.password', function (value) {
+                $scope.validate(value, 'passwordValidation');
+            });
+
+            $scope.validate = function(value, controlName) {
+                if($scope.isInvalid(value)) {
+                    $scope.activeErrors[controlName] = validationMessages[controlName];
+                } else {
+                    delete $scope.activeErrors[controlName];
+                }
+            };
+
+            $scope.isInvalid = function(value) {
+                return !value;
+            };
+
+            $scope.isAllControlIsInvalid = function() {
+                return !!Object.keys($scope.activeErrors).length;
+            }
     }]);
